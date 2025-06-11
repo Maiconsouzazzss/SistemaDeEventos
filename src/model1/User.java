9bc7008cdb4d1f818193e054037e065189fd5b2f
Maicon <maiconsouza.zzss@gmail.com>
2025-06-11 19:05:58 -0300

    feat: criar classe User com nome, email e cidade

---
+++ src/model1/User.java
@@ -0,0 +1,31 @@
+package model1;
+
+public class User {
+
+	private String name;
+	private String email;
+	private String city;
+	
+	public User(String name, String email, String city) {
+		this.name = name;
+		this.email = email;
+		this.city = city;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getCity() {
+		return city;
+	}
+	
+	@Override
+	public String toString() {
+		return "Usuário: " + name + " | Email: " + email + " | Cidade: " + city;
+	}
+}

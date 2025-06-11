1429a0bbc47ad3c574d327467f19a5fb3e665af8
Maicon <maiconsouza.zzss@gmail.com>
2025-06-11 19:33:19 -0300

    feat: criar classe EventManager com métodos iniciais

--- src/controller/EventManager.java
+++ src/controller/EventManager.java
@@ -1,5 +1,23 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model1.Event;
+
 public class EventManager {
 
+    private List<Event> events;
+
+    public EventManager() {
+        this.events = new ArrayList<>();
+    }
+
+    public void addEvent(Event event) {
+        events.add(event);
+    }
+
+    public List<Event> getAllEvents() {
+        return events;
+    }
 }

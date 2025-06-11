df43023d481cd8ef85db333ad746578b66f22da8
Maicon <maiconsouza.zzss@gmail.com>
2025-06-11 19:55:18 -0300

    feat: integrar FileHandler ao EventManager para persistência

--- src/controller/EventManager.java
+++ src/controller/EventManager.java
@@ -1,20 +1,21 @@
 package controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import model1.Event;
+import util.FileHandler;
 
 public class EventManager {
 
     private List<Event> events;
 
     public EventManager() {
-        this.events = new ArrayList<>();
+        this.events = FileHandler.loadEvents(); // carregar eventos ao iniciar
     }
 
     public void addEvent(Event event) {
         events.add(event);
+        FileHandler.saveEvents(events); // salvar sempre que adicionar
     }
 
     public List<Event> getAllEvents() {
